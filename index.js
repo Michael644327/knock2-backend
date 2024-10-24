4aed66543260c12a08db08627845530c7151516b
Michael644327 <keinsacer@gmail.com>
2024-10-25 00:31:29 +0800

    static 2

--- index.js
+++ index.js
@@ -24,8 +24,6 @@ const app = express();
 //   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
 // }));
 
-app.use('/images', express.static(path.join(__dirname, 'public/images')));
-
 app.use(cors({
   origin: ['http://localhost:3000', 'https://knock2-frontend-3qms.vercel.app'],
   methods: ['GET','POST','PUT','DELETE','OPTIONS'],
